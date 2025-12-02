511eee942359aac8b4b87b0215290ad328d98f92
SirYorch <jcuevam3@est.ups.edu.ec>
2025-12-02 16:15:26 -0500

    Push avatar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+import { useRef } from "react";
+
+function App() {
+
+  const avatarRef = useRef();
+
+
+  return (
+    <>
+      <Loader />
+      <Leva/>
+      <UI /> 
+      <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
+        <Experience />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
